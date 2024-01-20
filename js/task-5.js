0216159fa9a8cdec382e3c1c52c71dd41857e71b
EProko <lokki9526@gmail.com>
2024-01-21 01:27:31 +0200

    Ready #5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
+const btnChangeColor = document.querySelector(".change-color");
+
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+btnChangeColor.addEventListener("click", function () {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = getRandomHexColor();
+
+  const span = document.querySelector(".color");
+  span.textContent = body.style.backgroundColor;
+});
