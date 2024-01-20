be32e1235b5a8598c5e79e6e6d850ef2d095cbc0
EProko <lokki9526@gmail.com>
2024-01-20 23:14:52 +0200

    ready #4

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,10 @@
 const Input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 const listenerForInput = (event) => {
-  if (Input.textContent.trim() != " ") {
-    output.textContent = event.currentTarget.value.trim();
-  } else {
+  if (event.currentTarget.value.trim() === "") {
     output.textContent = "Anonymous";
+  } else {
+    output.textContent = event.currentTarget.value.trim();
   }
 };
 Input.addEventListener("input", listenerForInput);
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+const listenerForInput = (event) => {
+  event.preventDefault();
+  const liginInform = event.target;
+  const Inform = {
+    Email: `${liginInform.elements.email.value.trim()}`,
+    Password: `${liginInform.elements.password.value.trim()}`,
+  };
+  const email = liginInform.elements.email.value.trim();
+  const password = liginInform.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  }
+
+  console.log(Inform);
+  liginInform.reset();
+};
+
+loginForm.addEventListener("submit", listenerForInput);
